15ff399c948469561fbf35b49faa606b80642a92
G <g.singh@Gs-MacBook-Air.local>
2023-03-26 21:43:09 -0400

    routing added for user to be navigated to UserDetails page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { Login } from "./Login";
-import { Register } from "./Register"
-import { useState } from 'react';
+import { Register } from "./Register";
 import { UserDetails } from './UserDetails';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
-
-
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
-    <div className="App">
-
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/UserDetails" element={<UserDetails />} />
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,30 +1,32 @@
 import React, { useState } from "react";
+import { navigate } from "react-dom";
 
 export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [name, setName] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-        console.log(pass);
-        console.log(name);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(pass);
+    console.log(name);
+    navigate('/user-details'); // navigate to UserDetails page
+  }
 
-    return (
-        <div className="auth-form-container">
-            <h1 className="heading"> Register page </h1>
-            <form className="register-form" onSubmit={handleSubmit}>
-                <label htmlFor="name"> Full name: </label>
-                <input value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="full name" />
-                <label htmlfor="email"> Email: </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@address.com" id="email" name="email" />
-                <label htmlfor="password"> Password: </label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} placeholder="********" id="password" name="password" />
-                <button type="submit">LOGIN</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('login')}> Already have an account? Login here. </button>
-        </div>
-    )
+  return (
+    <div className="auth-form-container">
+      <h1 className="heading"> Register page </h1>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name"> Full name: </label>
+        <input value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="full name" />
+        <label htmlFor="email"> Email: </label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@address.com" id="email" name="email" />
+        <label htmlFor="password"> Password: </label>
+        <input value={pass} onChange={(e) => setPass(e.target.value)} placeholder="********" id="password" name="password" />
+        <button type="submit">REGISTER</button>
+      </form>
+      <button className="link-btn" onClick={() => navigate('/login')}> Already have an account? Login here. </button>
+    </div>
+  )
 }
